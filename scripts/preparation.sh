2ccdbc6c24b450b873ba55877066fdd535887bea
Komei1009 <1994komei1009@gmail.com>
2022-05-19 23:22:07 +0900

    修正

--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -3,13 +3,13 @@ find /var/www/app -type f -exec chmod 664 {} \;
 find /var/www/app -type d -exec chmod 774 {} \;
 chown -R ec2-user:nginx /var/www/app
 
-chmod -R 775 /var/www/app/src/storage
-chmod -R 775 /var/www/app/src/bootstrap/cache
+chmod -R 775 /var/www/app/storage
+chmod -R 775 /var/www/app/bootstrap/cache
 
 # db migrate
- cd /var/www/app/src
+ cd /var/www/app
  php artisan migrate
 
 # composer
- cd /var/www/app/src
+ cd /var/www/app
  composer install
