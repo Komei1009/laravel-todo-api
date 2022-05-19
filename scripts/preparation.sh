5eb11e12570083a81926636e375e71c423885d1a
Komei1009 <1994komei1009@gmail.com>
2022-05-19 17:11:50 +0900

    push

--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo find /var/www/app -type f -exec chmod 664 {} \;
-sudo find /var/www/app -type d -exec chmod 774 {} \;
-sudo chown -R ec2-user:nginx /var/www/app
+find /var/www/app -type f -exec chmod 664 {} \;
+find /var/www/app -type d -exec chmod 774 {} \;
+chown -R ec2-user:nginx /var/www/app
 
-sudo chmod -R 775 /var/www/app/src/storage
-sudo chmod -R 775 /var/www/app/src/bootstrap/cache
+chmod -R 775 /var/www/app/src/storage
+chmod -R 775 /var/www/app/src/bootstrap/cache
 
 # db migrate
-cd /var/www/app
-php artisan migrate
+# cd /var/www/app
+# php artisan migrate
 
 # composer
 # cd /var/www/app
