f5214851d6f34e04bd876879a4752642407d64b1
Komei1009 <1994komei1009@gmail.com>
2022-05-19 17:09:36 +0900

    test

--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#find /var/www/app -type f -exec chmod 664 {} \;
-#find /var/www/app -type d -exec chmod 774 {} \;
-#chown -R ec2-user:nginx /var/www/app
-#
-#chmod -R 775 /var/www/app/src/storage
-#chmod -R 775 /var/www/app/src/bootstrap/cache
-#
-## db migrate
-#cd /var/www/app
-#php artisan migrate
+sudo find /var/www/app -type f -exec chmod 664 {} \;
+sudo find /var/www/app -type d -exec chmod 774 {} \;
+sudo chown -R ec2-user:nginx /var/www/app
+
+sudo chmod -R 775 /var/www/app/src/storage
+sudo chmod -R 775 /var/www/app/src/bootstrap/cache
+
+# db migrate
+cd /var/www/app
+php artisan migrate
 
 # composer
 # cd /var/www/app
