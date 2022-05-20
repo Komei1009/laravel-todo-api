7546f9f8853046a3e7f7d7147c63024d7308dfb8
Komei1009 <1994komei1009@gmail.com>
2022-05-20 17:23:44 +0900

    test

--- deploy/preparation.sh
+++ deploy/preparation.sh
@@ -9,7 +9,7 @@ chown -R ec2-user:nginx /var/www/app
 ## db migrate
 # # shellcheck disable=SC2164
  cd /var/www/app
-# sudo -u nginx php artisan migrate --force
+ sudo -u nginx php artisan migrate --force
 #
 ## composer
 # # shellcheck disable=SC2164
