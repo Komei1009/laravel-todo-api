6b28de31e0b9c0cdf81e972ba52f6221ce626bbb
Komei1009 <1994komei1009@gmail.com>
2022-05-20 17:16:33 +0900

    処理なし

--- deploy/preparation.sh
+++ deploy/preparation.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-find /var/www/app -type f -exec chmod 664 {} \;
-find /var/www/app -type d -exec chmod 774 {} \;
-chown -R ec2-user:nginx /var/www/app
-
-#chmod -R 775 /var/www/app/storage
-#chmod -R 775 /var/www/app/bootstrap/cache
-
-# db migrate
- # shellcheck disable=SC2164
- cd /var/www/app
- sudo -u nginx php artisan migrate
-
-# composer
- # shellcheck disable=SC2164
- sudo -u nginx composer install
+#find /var/www/app -type f -exec chmod 664 {} \;
+#find /var/www/app -type d -exec chmod 774 {} \;
+#chown -R ec2-user:nginx /var/www/app
+#
+##chmod -R 775 /var/www/app/storage
+##chmod -R 775 /var/www/app/bootstrap/cache
+#
+## db migrate
+# # shellcheck disable=SC2164
+# cd /var/www/app
+# sudo -u nginx php artisan migrate
+#
+## composer
+# # shellcheck disable=SC2164
+# sudo -u nginx composer install
