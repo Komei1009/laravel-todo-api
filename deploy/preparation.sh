b00ea0a156b7fa72f38241e947b3fcd36da12647
Komei1009 <1994komei1009@gmail.com>
2022-05-20 16:53:01 +0900

    test

--- deploy/preparation.sh
+++ deploy/preparation.sh
@@ -3,6 +3,9 @@ find  /var/www/app -type f -exec chmod 664 {} \;
 find /var/www/app -type d -exec chmod 774 {} \;
 chown -R ec2-user:nginx /var/www/app
 
+chmod -R 775 /var/www/app/storage
+chmod -R 775 /var/www/app/bootstrap/cache
+
 # db migrate
  # shellcheck disable=SC2164
  cd /var/www/app
